a917762bac9fdd336b25a20cd5dfca9e0614558f
Blake Johnson <blake41@gmail.com>
2015-05-04 17:16:47 -0400

    check that click on the button shows me last game

--- spec/tictactoe.spec.js
+++ spec/tictactoe.spec.js
@@ -106,6 +106,7 @@ describe('javascript', function() {
     it("should print out the results of the last game in an alert", function() {
       setFixtures('<body><table border="1" cellpadding="40"><tr><td data-x="0", data-y="0"></td><td data-x="1", data-y="0"></td><td data-x="2", data-y="0"></td></tr><tr><td data-x="0", data-y="1"></td><td data-x="1", data-y="1"></td><td data-x="2", data-y="1"></td></tr><tr><td data-x="0", data-y="2"></td><td data-x="1", data-y="2"></td><td data-x="2", data-y="2"></td></tr></table><button id="lastGame">Show Me Last Games Results!</button><div id="lastGameBox"></div></body>')
       attachListeners()
+      spyOn(window, "")
       // x goes
       var selector = '[data-x="0"][data-y="0"]'
       $(selector).click()
@@ -121,6 +122,9 @@ describe('javascript', function() {
       // x goes
       var selector = '[data-x="2"][data-y="2"]'
       $(selector).click()
+      $("#lastGame").click()
+      
+      expect(window.alert)
       expect(lastGameString()).toEqual("XOO\n-X-\n--X\n");     
     })
   });
