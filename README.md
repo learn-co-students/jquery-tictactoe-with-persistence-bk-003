f70da60e39a8a55c770544e0d9d4b35f9b83e612
Katie Hoffman <ktahoffman@gmail.com>
2015-05-18 15:19:35 -0400

    add jquery, add semicolons to test suite

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
-you must have a function called attachlisteners which I call in the tests to attach the click handlers to the page after I've mocked up the DOM
+# jQuery Tic-Tac-Toe with Persistence
 
-for reasons katie and i couldn't figure out you must wrap your handler in an anonymous function and manually call the function that handles the event
-var attachListeners = function() {
-  $("tbody").click(function(event) {
-    doTurn(event)
-  })
-}
+## About
 
-In the last version of this you had to only keep track of the state of the most current game.  This time you'll have to keep track of the game that was most recently played.  When someone wins, or ties, you should save the state of the game.  We're not going to tell you how to do this.  This SHOULD be hard and you might have to do some "hacky" stuff to get it to work.  The new tests this time only test that there's a button I can click that will pop up a view of the state of the board at the end of last game.  We're intentionally not going to guide or test HOW you implement that functionality.
+In the first jQuery tic-tac-toe lab, you had to only keep track of the state of the most current game.
+
+This time you'll have to keep track of the game that was most recently played. When someone wins, or ties, you should save the state of the game.  We're not going to tell you how to do this. This SHOULD be hard and you might have to do some "hacky" stuff to get it to work.
+
+## Test Suite
+
+Feel free to use the code you already wrote for jQuery Tic-Tac-Toe to pass the first seven tests.
+
+The new tests this time only test that there's a button I can click that will pop up a view of the state of the board at the end of last game. We're intentionally not going to guide or test HOW you implement that functionality.
--- spec/tictactoe.spec.js
+++ spec/tictactoe.spec.js
@@ -1,50 +1,79 @@
 'use strict';
+
 describe('javascript', function() {
   beforeEach(function(){
     turn = 0;
-  })
+  });
 
   describe( "#attachListeners", function() {
     it("should attach event listeners which call your turn function when someone clicks on a square", function() {
-      setFixtures('<body><table border="1" cellpadding="40"><tr><td data-x="0", data-y="0"></td><td data-x="1", data-y="0"></td><td data-x="2", data-y="0"></td></tr><tr><td data-x="0", data-y="1"></td><td data-x="1", data-y="1"></td><td data-x="2", data-y="1"></td></tr><tr><td data-x="0", data-y="2"></td><td data-x="1", data-y="2"></td><td data-x="2", data-y="2"></td></tr></table></body>')
-      attachListeners()
-      var selector = '[data-x="0"][data-y="0"]'
-      spyOn(window, "doTurn")
-      $(selector).click()
-      expect(window.doTurn).toHaveBeenCalled()
+      setFixtures('<body><table border="1" cellpadding="40"><tr><td data-x="0", data-y="0"></td><td data-x="1", data-y="0"></td><td data-x="2", data-y="0"></td></tr><tr><td data-x="0", data-y="1"></td><td data-x="1", data-y="1"></td><td data-x="2", data-y="1"></td></tr><tr><td data-x="0", data-y="2"></td><td data-x="1", data-y="2"></td><td data-x="2", data-y="2"></td></tr></table></body>');
+      attachListeners();
+      var selector = '[data-x="0"][data-y="0"]';
+      spyOn(window, "doTurn");
+      $(selector).click();
+      expect(window.doTurn).toHaveBeenCalled();
     });     
   });
 
   describe( "#doTurn", function() {
-    it("should create an array of 9 empty values", function() {
+    var myEvent;
+    beforeEach(function(){
+      myEvent = $('[data-x="0"][data-y="0"]').click();
+    });
+
+    it("should increment turn and call on `checkWinner()`, `updateState()`", function() {
       spyOn(window, "checkWinner");
       spyOn(window, "updateState");
-      doTurn()
-      expect(turn).toEqual(1)
-      expect(window.updateState).toHaveBeenCalled
-      expect(window.checkWinner).toHaveBeenCalled
-    });     
-  });
-
-  describe( "#updateState", function() {
-    it("should add the mark of the player that moved into the correct index in the array and tell the board to update its state", function() {
-      setFixtures('<body><table border="1" cellpadding="40"><tr><td data-x="0", data-y="0"></td><td data-x="1", data-y="0"></td><td data-x="2", data-y="0"></td></tr><tr><td data-x="0", data-y="1"></td><td data-x="1", data-y="1"></td><td data-x="2", data-y="1"></td></tr><tr><td data-x="0", data-y="2"></td><td data-x="1", data-y="2"></td><td data-x="2", data-y="2"></td></tr></table></body>')
-      attachListeners()
-      var selector = '[data-x="0"][data-y="0"]'
-      $(selector).click()
-      expect($(selector).html()).toEqual("X")
+      doTurn(myEvent);
+      expect(turn).toEqual(1);
+      expect(window.updateState).toHaveBeenCalled;
+      expect(window.checkWinner).toHaveBeenCalled;
     });     
   });
 
   describe( "#player", function() {
     it("should return the mark of the current player", function() {
-      expect(player()).toEqual("X")
+      expect(player()).toEqual("X");
+    });     
+    it("should return the mark of the current player", function() {
+      turn = 1;
+      expect(player()).toEqual("O");
+    });
+  });
+
+  describe( "#updateState", function() {
+    it("adds the return value of `player()` to the clicked `td` on the board", function() {
+      setFixtures('<body><table border="1" cellpadding="40"><tr><td data-x="0", data-y="0"></td><td data-x="1", data-y="0"></td><td data-x="2", data-y="0"></td></tr><tr><td data-x="0", data-y="1"></td><td data-x="1", data-y="1"></td><td data-x="2", data-y="1"></td></tr><tr><td data-x="0", data-y="2"></td><td data-x="1", data-y="2"></td><td data-x="2", data-y="2"></td></tr></table></body>');
+      attachListeners();
+      var selector = '[data-x="0"][data-y="0"]';
+      $(selector).click();
+      expect($(selector).html()).toEqual("X");
+    }); 
+
+    it("calls on `player()`", function() {
+      setFixtures('<body><table border="1" cellpadding="40"><tr><td data-x="0", data-y="0"></td><td data-x="1", data-y="0"></td><td data-x="2", data-y="0"></td></tr><tr><td data-x="0", data-y="1"></td><td data-x="1", data-y="1"></td><td data-x="2", data-y="1"></td></tr><tr><td data-x="0", data-y="2"></td><td data-x="1", data-y="2"></td><td data-x="2", data-y="2"></td></tr></table></body>');
+      attachListeners();
+      spyOn(window, "player");
+      var selector = '[data-x="1"][data-y="0"]';
+      $(selector).click();
+      expect(window.player).toHaveBeenCalled;
+    });     
+  });
+
+  describe( "#message", function() {
+    it("adds the string it's passed to the div with an id of message", function() {
+      setFixtures('<body><table border="1" cellpadding="40"><tr><td data-x="0", data-y="0"></td><td data-x="1", data-y="0"></td><td data-x="2", data-y="0"></td></tr><tr><td data-x="0", data-y="1"></td><td data-x="1", data-y="1"></td><td data-x="2", data-y="1"></td></tr><tr><td data-x="0", data-y="2"></td><td data-x="1", data-y="2"></td><td data-x="2", data-y="2"></td></tr></table><div id="message"></div></body>');
+      attachListeners();
+      var string = "Player X Won!";
+      message(string);
+      expect($("#message").html()).toEqual(string);
     });     
   });
 
   describe( "#checkWinner", function() {
     it("should tell me if there is a winning combo on the board for the current player", function() {
-      setFixtures('<body><table border="1" cellpadding="40"><tr><td data-x="0", data-y="0"></td><td data-x="1", data-y="0"></td><td data-x="2", data-y="0"></td></tr><tr><td data-x="0", data-y="1"></td><td data-x="1", data-y="1"></td><td data-x="2", data-y="1"></td></tr><tr><td data-x="0", data-y="2"></td><td data-x="1", data-y="2"></td><td data-x="2", data-y="2"></td></tr></table></body>')
+      setFixtures('<body><table border="1" cellpadding="40"><tr><td data-x="0", data-y="0"></td><td data-x="1", data-y="0"></td><td data-x="2", data-y="0"></td></tr><tr><td data-x="0", data-y="1"></td><td data-x="1", data-y="1"></td><td data-x="2", data-y="1"></td></tr><tr><td data-x="0", data-y="2"></td><td data-x="1", data-y="2"></td><td data-x="2", data-y="2"></td></tr></table></body>');
       attachListeners()
       spyOn(window, "updateState");
       var selector = '[data-x="0"][data-y="0"]'
@@ -53,77 +82,73 @@ describe('javascript', function() {
       $(selector).click()
       expect(checkWinner()).toEqual(false)   
     });     
-  });
 
-  describe( "#checkWinner", function() {
     it("should tell me if there is a winning combo on the board for the current player (vertical)", function() {
-      setFixtures('<body><table border="1" cellpadding="40"><tr><td data-x="0", data-y="0"></td><td data-x="1", data-y="0"></td><td data-x="2", data-y="0"></td></tr><tr><td data-x="0", data-y="1"></td><td data-x="1", data-y="1"></td><td data-x="2", data-y="1"></td></tr><tr><td data-x="0", data-y="2"></td><td data-x="1", data-y="2"></td><td data-x="2", data-y="2"></td></tr></table></body>')
-      attachListeners()
+      setFixtures('<body><table border="1" cellpadding="40"><tr><td data-x="0", data-y="0"></td><td data-x="1", data-y="0"></td><td data-x="2", data-y="0"></td></tr><tr><td data-x="0", data-y="1"></td><td data-x="1", data-y="1"></td><td data-x="2", data-y="1"></td></tr><tr><td data-x="0", data-y="2"></td><td data-x="1", data-y="2"></td><td data-x="2", data-y="2"></td></tr></table></body>');
+      attachListeners();
       spyOn(window, "message");
       // x goes
-      var selector = '[data-x="0"][data-y="0"]'
-      $(selector).click()
+      var selector = '[data-x="0"][data-y="0"]';
+      $(selector).click();
       // y goes
-      var selector = '[data-x="1"][data-y="0"]'
-      $(selector).click()
+      var selector = '[data-x="1"][data-y="0"]';
+      $(selector).click();
       // x goes
-      var selector = '[data-x="0"][data-y="1"]'
-      $(selector).click()
+      var selector = '[data-x="0"][data-y="1"]';
+      $(selector).click();
       // y goes
-      var selector = '[data-x="2"][data-y="0"]'
-      $(selector).click()
+      var selector = '[data-x="2"][data-y="0"]';
+      $(selector).click();
       // x goes
-      var selector = '[data-x="0"][data-y="2"]'
-      $(selector).click()
-      expect(window.message).toHaveBeenCalledWith("Player X Won!")
+      var selector = '[data-x="0"][data-y="2"]';
+      $(selector).click();
+      expect(window.message).toHaveBeenCalledWith("Player X Won!");
     });     
-  });
 
-  describe( "#checkWinner", function() {
     it("should tell me if there is a winning combo on the board for the current player (diagonal)", function() {
-      setFixtures('<body><table border="1" cellpadding="40"><tr><td data-x="0", data-y="0"></td><td data-x="1", data-y="0"></td><td data-x="2", data-y="0"></td></tr><tr><td data-x="0", data-y="1"></td><td data-x="1", data-y="1"></td><td data-x="2", data-y="1"></td></tr><tr><td data-x="0", data-y="2"></td><td data-x="1", data-y="2"></td><td data-x="2", data-y="2"></td></tr></table></body>')
+      setFixtures('<body><table border="1" cellpadding="40"><tr><td data-x="0", data-y="0"></td><td data-x="1", data-y="0"></td><td data-x="2", data-y="0"></td></tr><tr><td data-x="0", data-y="1"></td><td data-x="1", data-y="1"></td><td data-x="2", data-y="1"></td></tr><tr><td data-x="0", data-y="2"></td><td data-x="1", data-y="2"></td><td data-x="2", data-y="2"></td></tr></table></body>');
       attachListeners()
       spyOn(window, "message");
       // x goes
-      var selector = '[data-x="0"][data-y="0"]'
-      $(selector).click()
+      var selector = '[data-x="0"][data-y="0"]';
+      $(selector).click();
       // y goes
-      var selector = '[data-x="1"][data-y="0"]'
-      $(selector).click()
+      var selector = '[data-x="1"][data-y="0"]';
+      $(selector).click();
       // x goes
-      var selector = '[data-x="1"][data-y="1"]'
-      $(selector).click()
+      var selector = '[data-x="1"][data-y="1"]';
+      $(selector).click();
       // y goes
-      var selector = '[data-x="2"][data-y="0"]'
-      $(selector).click()
+      var selector = '[data-x="2"][data-y="0"]';
+      $(selector).click();
       // x goes
-      var selector = '[data-x="2"][data-y="2"]'
-      $(selector).click()
+      var selector = '[data-x="2"][data-y="2"]';
+      $(selector).click();
       expect(window.message).toHaveBeenCalledWith("Player X Won!")    });     
   });
 
   describe( "#showLastGame", function() {
     it("should print out the results of the last game in an alert", function() {
-      setFixtures('<body><table border="1" cellpadding="40"><tr><td data-x="0", data-y="0"></td><td data-x="1", data-y="0"></td><td data-x="2", data-y="0"></td></tr><tr><td data-x="0", data-y="1"></td><td data-x="1", data-y="1"></td><td data-x="2", data-y="1"></td></tr><tr><td data-x="0", data-y="2"></td><td data-x="1", data-y="2"></td><td data-x="2", data-y="2"></td></tr></table><button id="lastGame">Show Me Last Games Results!</button><div id="lastGameBox"></div></body>')
-      attachListeners()
-      spyOn(window, "showLastGame")
+      setFixtures('<body><table border="1" cellpadding="40"><tr><td data-x="0", data-y="0"></td><td data-x="1", data-y="0"></td><td data-x="2", data-y="0"></td></tr><tr><td data-x="0", data-y="1"></td><td data-x="1", data-y="1"></td><td data-x="2", data-y="1"></td></tr><tr><td data-x="0", data-y="2"></td><td data-x="1", data-y="2"></td><td data-x="2", data-y="2"></td></tr></table><button id="lastGame">Show Me Last Games Results!</button><div id="lastGameBox"></div></body>');
+      attachListeners();
+      spyOn(window, "showLastGame");
       // x goes
-      var selector = '[data-x="0"][data-y="0"]'
-      $(selector).click()
+      var selector = '[data-x="0"][data-y="0"]';
+      $(selector).click();
       // y goes
-      var selector = '[data-x="1"][data-y="0"]'
-      $(selector).click()
+      var selector = '[data-x="1"][data-y="0"]';
+      $(selector).click();
       // x goes
-      var selector = '[data-x="1"][data-y="1"]'
-      $(selector).click()
+      var selector = '[data-x="1"][data-y="1"]';
+      $(selector).click();
       // y goes
-      var selector = '[data-x="2"][data-y="0"]'
-      $(selector).click()
+      var selector = '[data-x="2"][data-y="0"]';
+      $(selector).click();
       // x goes
-      var selector = '[data-x="2"][data-y="2"]'
-      $(selector).click()
-      $("#lastGame").click()
-      expect(window.showLastGame).toHaveBeenCalled
+      var selector = '[data-x="2"][data-y="2"]';
+      $(selector).click();
+      $("#lastGame").click();
+      expect(window.showLastGame).toHaveBeenCalled();
       expect(lastGameString()).toEqual("XOO\n-X-\n--X\n");     
     })
   });
